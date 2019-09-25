bc1bf0c2ca3ab42d9a7e0df554dca7f69e8d0e2d
jaybenaim <benaimjacob@gmail.com>
2019-09-25 11:24:57 -0400

    added about me section

--- app/javascript/components/AboutMe.js
+++ app/javascript/components/AboutMe.js
@@ -4,7 +4,37 @@ class AboutMe extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <section className="about-me-section"></section>
+        <section className="about-me-section">
+          <p>
+            I began my journey as a software developer from a chef. I spent most
+            my working life in the kitchen, building temas, creating menus,
+            living a very fast paced life. Unfortunately I was in a motorcycle
+            accident where I was not able to return to my culinary feild. I was
+            yearning for something to expand my creativity, I had a passion for
+            technology growing up, I even got an award in grade 8 for helping
+            with the technology so often. I knew I could find something out
+            there where I could create and enjoy what I did. I began to discover
+            all these free online resources where one could build a career in a
+            couple of lessons. I was definitely intrigued by this statement, I
+            decided to dig in deeper. I was taking every free course I could get
+            my hands on, doing tutorial after tutorial. Finally, a few months
+            later I decided that yes this is what I want to do with the rest of
+            my life. I started to reach out to individuals in my community, and
+            started getting a conversation going. I found that alot of people in
+            the industry were fond of these bootcamps where you can enhance
+            yours skills with a 500-hour intense training camp, so I decided to
+            pursure that next. I started doing some research and started
+            visiting thsese schools to get a better feel of how it would be like
+            to be in this industry. Once I found my met I knew I was going to
+            build something great. Every day I put time in to work on something
+            that I enjoy making, whether it be something new or something I toss
+            out at the end of the day. I always try and build something new. I
+            love discovering new ways to do things, new methods, new languages,
+            things to make my life easier. I mean don't we all want to make life
+            a little easy sometimes. I know I have just begun but I cannot wait
+            to explore this indulging industry.
+          </p>
+        </section>
       </React.Fragment>
     );
   }
--- app/javascript/components/Skills.js
+++ app/javascript/components/Skills.js
@@ -6,51 +6,53 @@ class Skills extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div class="skills-container">
-          <h3 class="skills-heading">
+        <div className="skills-container">
+          <p className="skills-heading">
             <strong>Skills</strong>
-          </h3>
+          </p>
           <table>
-            <tr>
-              <td>Python</td>
-              <td>JavaScript</td>
-              <td>Django</td>
-              <td>React</td>
-            </tr>
-            <tr>
-              <td>
-                <SkillsProgressMeter text="Python" number="80" />
-              </td>
-              <td>
-                <SkillsProgressMeter text="JavaScript" number="80" />
-              </td>
-              <td>
-                <SkillsProgressMeter text="Django" number="70" />
-              </td>
-              <td>
-                <SkillsProgressMeter text="React" number="80" />
-              </td>
-            </tr>
-            <tr>
-              <td>Bootstrap</td>
-              <td>MongoDB</td>
-              <td>PostgreSQL</td>
-              <td>Jest</td>
-            </tr>
-            <tr>
-              <td>
-                <SkillsProgressMeter text="Bootstrap" number="90" />
-              </td>
-              <td>
-                <SkillsProgressMeter text="MongoDB" number="70" />
-              </td>
-              <td>
-                <SkillsProgressMeter text="PostgreSQL" number="80" />
-              </td>
-              <td>
-                <SkillsProgressMeter text="Jest" number="70" />
-              </td>
-            </tr>
+            <tbody>
+              <tr>
+                <td>Python</td>
+                <td>JavaScript</td>
+                <td>Django</td>
+                <td>React</td>
+              </tr>
+              <tr>
+                <td>
+                  <SkillsProgressMeter text="Python" number="80" />
+                </td>
+                <td>
+                  <SkillsProgressMeter text="JavaScript" number="80" />
+                </td>
+                <td>
+                  <SkillsProgressMeter text="Django" number="70" />
+                </td>
+                <td>
+                  <SkillsProgressMeter text="React" number="80" />
+                </td>
+              </tr>
+              <tr>
+                <td>Bootstrap</td>
+                <td>MongoDB</td>
+                <td>PostgreSQL</td>
+                <td>Jest</td>
+              </tr>
+              <tr>
+                <td>
+                  <SkillsProgressMeter text="Bootstrap" number="90" />
+                </td>
+                <td>
+                  <SkillsProgressMeter text="MongoDB" number="70" />
+                </td>
+                <td>
+                  <SkillsProgressMeter text="PostgreSQL" number="80" />
+                </td>
+                <td>
+                  <SkillsProgressMeter text="Jest" number="70" />
+                </td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </React.Fragment>
