b8c0a444648a7e79470905c0c135e2bd1db73799
jaybenaim <benaimjacob@gmail.com>
2019-10-09 15:38:23 -0400

    added layout for my projects page and added first four prjects

--- app/javascript/components/ProjectsLandingPage.js
+++ app/javascript/components/ProjectsLandingPage.js
@@ -6,23 +6,88 @@ class ProjectsLandingPage extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Container>
+        <Container className="project-landing">
+          <h1>My Projects</h1>
           <Row>
-            <Col lg={4}>1</Col>
-            <Col lg={4}>2</Col>
-            <Col lg={4}>3</Col>
+            <Col lg={4}>
+              <img
+                src="https://i.imgur.com/8pRxQan.jpg"
+                alt="snapshot of homepage for community app"
+              ></img>
+              <a href="https://github.com/jaybenaim/community">Community</a>
+              <p>
+                An app bringing communities together that allows users to see
+                the people in their community and create a communication base
+                where items can be lent to each other for a small price of a
+                conversation or a cup of coffee Two week sprint Utilized Github
+                project planning
+              </p>
+            </Col>
+            <Col lg={4}>
+              <img
+                src="https://i.imgur.com/yy9nXpg.jpg"
+                alt="snapshot of homepage for yelpcamp app"
+              ></img>
+              <a href="https://github.com/jaybenaim/yelpCamp/tree/master">
+                YelpCamp
+              </a>
+              <p>
+                Created a campsite blog-style web app Utilized non-relational
+                database
+              </p>
+            </Col>
+            <Col lg={4}>
+              <img
+                src="https://i.imgur.com/vZVKLNE.png"
+                alt="snapshot of homepage for crowd_surfer app"
+              ></img>
+              <a href="https://i.imgur.com/vZVKLNE.png">Crowd Surfer</a>
+              <p>
+                A crowd funding app using Django and Python, and PostgreSQL.
+                Group project of four.
+              </p>
+            </Col>
           </Row>
           <Row>
-            <Col lg={4}>4</Col>
-            <Col lg={4}>5</Col>
-            <Col lg={4}>6</Col>
+            <Col lg={4}>
+              <a href="https://github.com/jaybenaim/django-template">
+                Django Template
+              </a>
+              <p>
+                Built a template starter for DJango Integrated basic
+                authentication for users to sign up
+              </p>
+            </Col>
+            <Col lg={4}>
+              <a href=""> </a>
+              <p></p>
+            </Col>
+            <Col lg={4}></Col>
           </Row>
           <Row>
-            <Col lg={4}>7</Col>
-            <Col lg={4}>8</Col>
-            <Col lg={4}>9</Col>
+            <Col lg={4}></Col>
+            <Col lg={4}></Col>
+            <Col lg={4}></Col>
           </Row>
         </Container>
+        <div className="contact-info">
+          <p>
+            <a href="https://github.com/jaybenaim">Github: </a>
+            https://github.com/jaybenaim
+          </p>
+          <p>
+            <a href="https://www.linkedin.com/in/jacob-benaim-a64a0818a/">
+              LinkedIn:
+            </a>
+            https://www.linkedin.com/in/jacob-benaim-a64a0818a/
+          </p>
+          <p>
+            <i className="fa fa-envelope-square"></i> benaimJacob@gmail.com{" "}
+          </p>
+          <p>
+            <i className="fa fa-phone"></i> 647-640-4714
+          </p>
+        </div>
       </React.Fragment>
     );
   }
